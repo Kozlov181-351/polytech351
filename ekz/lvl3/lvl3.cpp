30ab028f7a8efd100b1acd7842821c4a58f8837b
Kozlov181-351 <gitgit2019@mail.ru>
2019-03-16 13:51:43 +0300

    17.03.2019

--- ekz/lvl3/lvl3.cpp
+++ ekz/lvl3/lvl3.cpp
@@ -1,34 +1,51 @@
 ﻿// lvl3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 //
 
-#include "pch.h"
-#include <iostream>
-
-int main()
-{
-#include <iostream>
-#include <string>
-#include <stdio.h>
-#include <stdlib.h>
-
+#include "pch.h" 
+#include <iostream> 
+#include <string> 
 	using namespace std;
-
-	void e(int c) {
-		if (c % 40 == 0)
-			cout << endl;
-	}
-
 	int main()
 	{
-		fstream fileopen, filedecod;
-		fileopen.open("INPUT.txt", std::fstream::in); // открываем их опять бинарными
-		filedecod.open("whitebearcoming.txt", std::fstream::out);
-
-		while (fileopen.gcount() > 0)
- if ()
-		
+		string word;
+		cin >> word;
+		string temp;
+		char t = word[0];
+		int count = 1;
+		for (int i = 1; i < word.length(); i++)
+		{
+			if (word[i] == t)
+			{
+				count++;
+			}
+			else {
+				if (count == 1) {
+					temp += t;
+					t = word[i];
+				}
+				else
+				{
+					temp += to_string(count);
+					temp += t;
+					t = word[i];
+					count = 1;
+				}
+			}
+			if (i == word.length() - 1)
+			{
+				if (count == 1) {
+					temp += t;
+					t = word[i];
+				}
+				else
+				{
+					temp += to_string(count);
+					temp += t;
+					t = word[i];
+					count = 1;
+				}
+			}
 		}
+		cout << temp;
 	}
-
-}
 
---
+++ ekz/lvl4v2/lvl4v2.cpp
@@ -0,0 +1,32 @@
+﻿// lvl4v2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "pch.h"
+#include <iostream>
+using namespace std; 
+
+int func(int dd)
+{
+	if (dd == 1)
+	{
+		return 1;
+	}
+	else if (dd == 0)
+	{
+		return 0;
+	}
+	else if (dd >= 2 && dd % 2 == 0) {
+		return func(dd / 2);
+	}
+	else if (dd >= 2 && dd % 2 == 1) {
+		return func((dd - 1) / 2) + func((dd - 1 / 2) + 1);
+	}
+};
+int main()
+{
+	int n;
+	cin >> n;
+	cout << func(n);
+};
+
+	
