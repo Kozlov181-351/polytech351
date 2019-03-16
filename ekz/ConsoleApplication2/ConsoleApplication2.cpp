4d198a05ba46127d34daef24d118ddc5614f7a86
Kozlov181-351 <gitgit2019@mail.ru>
2019-03-16 13:36:14 +0300

    17.03.2019

--- ekz/ConsoleApplication2/ConsoleApplication2.cpp
+++ ekz/ConsoleApplication2/ConsoleApplication2.cpp
@@ -3,36 +3,48 @@
 
 #include "pch.h"
 #include <iostream>
+#include <openssl/conf.h> //функции,
+#include <openssl/evp.h>
+#include <openssl/err.h>
+#include <openssl/aes.h>
+#include <fstream>
+#pragma comment (lib, "libcrypto.lib")
+using namespace std;
 
 int main()
 {
-	fstream filecryingmore, whitebearcoming;// файлы для шифра и дешифра
+	unsigned char *key = (unsigned char *)"019283745"; //пароль (ключ)
+	unsigned char *iv = (unsigned char *)"987654321054321"; //инициализирующий вектор
+	char buffer[256] = { 0 }; // буфер для перегонки.
+	char * out_buf[256] = { 0 }; // второй буфер для перегонки на выход. 
+	unsigned int number_of_bytes = 0; // это то, сколько он уже перегнал.  
+	int len = 0; //это длинна шифра
+	fstream filecry, filedecry;// файлы для шифра и дешифра
 
-	filecryingmore.open("filecry.txt", std::fstream::in | std::fstream::binary); // открываем их опять бинарными
-	whitebearcoming.open("whitebearcoming.txt", std::fstream::out | std::fstream::binary);
+	filecry.open("filecry.txt", std::fstream::in | std::fstream::binary); // открываем их бинарными
+	filedecry.open("filedecry.txt", std::fstream::out | std::fstream::binary);
 
-
+	EVP_CIPHER_CTX *ctx; //объявляем ссылку на структуру с настройками
 	ctx = EVP_CIPHER_CTX_new();
-
 	EVP_DecryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv);
 
-	filecryingmore.read(buffer, 256);
-	while (filecryingmore.gcount() > 0) // пошла вторая перегоночка из шифра и буквы
+	filecry.read(buffer, 256);
+	while (filecry.gcount() > 0) // дешифровка до момента пока в файле не останется ничего
 	{
 		// расшифровка 
-		EVP_DecryptUpdate(ctx, (unsigned char *)out_buf, &len, (unsigned char *)buffer, filecryingmore.gcount());
+		EVP_DecryptUpdate(ctx, (unsigned char *)out_buf, &len, (unsigned char *)buffer, filecry.gcount());
 
 		// запись расшифровки в файл 
-		whitebearcoming.write((const char *)out_buf, len);
+		filedecry.write((const char *)out_buf, len);
 
 		// чтение следующей порции 
-		filecryingmore.read(buffer, 256);
+		filecry.read(buffer, 256);
 	}
 
-	EVP_DecryptFinal_ex(ctx, (unsigned char *)out_buf, &len); //если в первый раз у нас была шлифовка пивком, то тут получается зачистка пивком? надо подумать
-	whitebearcoming.write((const char *)out_buf, len);
+	EVP_DecryptFinal_ex(ctx, (unsigned char *)out_buf, &len); //финализация дешифрования
+	filedecry.write((const char *)out_buf, len);
 
-	whitebearcoming.close();
-	filecryingmore.close();
+	filedecry.close();
+	filecry.close();
 }
 
